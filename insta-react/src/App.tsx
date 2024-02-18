6fde50087c7c3c4b7c29b60a561e2676618c119a
Anup <anuup22@outlook.com>
2024-02-18 19:07:44 +0530

    routing is created

---
+++ insta-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+
+    </main>
+  )
+}
+
+export default App
