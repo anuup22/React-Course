0bf75500f7ef03bf3e84d2801c51f1e4fe820c3a
Anup <anuup22@outlook.com>
2024-02-18 19:09:16 +0530

    routing is created

--- insta-react/src/App.tsx
+++ insta-react/src/App.tsx
@@ -4,7 +4,13 @@ import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
     <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route path='/sign-in' element={<SigninForm />} />
 
+        {/* private routes */}
+        <Route index element={<Home />} />  //index is the starting point/page of the app
+      </Routes>
     </main>
   )
 }
